357d2275a3aec8b3171e412a331ee9bca24a6ae8
Abdeahrouch <abdeahrouchabbassi@gmail.com>
2023-06-16 12:01:55 +0200

    local-storage-good

--- STARTER-webpack/src/App/App.js
+++ STARTER-webpack/src/App/App.js
@@ -1,84 +1,57 @@
-// Importer la configuration depuis le fichier app.config.json
 import config from "../../app.config.json";
-
-// Importer la bibliothèque mapbox
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../assets/style.css";
 
-// Définir la classe App
 class App {
-  // Propriétés
-
-  elDivMap; // Conteneur de la carte
-  map; // Instance de la carte
+  elDivMap;
+  map;
   form;
 
-  // Démarrer l'application
   start() {
     console.log("App démarrée...");
-
-    this.loadDom(); // Charger les éléments du DOM
-    this.initMap(); // Initialiser la carte
-    this.addFormulaire(); // Ajouter le formulaire
+    this.loadDom();
+    this.initMap();
+    this.addForm();
+    this.loadSavedReminders();
   }
 
-  // Méthode pour initialiser la carte
   initMap() {
-    // Utiliser la clé d'accès de l'API Mapbox GL à partir du fichier de configuration
     mapboxgl.accessToken = config.apis.mapbox_gl.api_key;
-
     this.map = new mapboxgl.Map({
-      container: this.elDivMap, // ID du conteneur de la carte
-      style: config.apis.mapbox_gl.map_styles.streets, // URL du style de la carte
-      center: [2.79, 42.68], // Coordonnées initiales [long, lat]
-      zoom: 12, // Niveau de zoom initial
+      container: this.elDivMap,
+      style: config.apis.mapbox_gl.map_styles.streets,
+      center: [2.79, 42.68],
+      zoom: 12,
     });
 
     const nav = new mapboxgl.NavigationControl();
     this.map.addControl(nav, "top-left");
-
-    // Écouter les événements de clic sur la carte
-    this.map.on("click", this.handleClickMap.bind(this));
+    this.map.on("click", this.handleMapClick.bind(this));
   }
 
-  // Charger les éléments du DOM
   loadDom() {
-    // --- MAP ---
-
     this.elDivMap = document.createElement("div");
     this.elDivMap.id = "map";
-    this.elDivMap.classList.add("map-container"); // Ajouter la classe pour la hauteur de la carte
+    this.elDivMap.classList.add("map-container");
     document.body.appendChild(this.elDivMap);
   }
 
-  // Méthode qui gère les clics sur la carte
-  handleClickMap(evt) {
-    console.log(evt);
-
-    // Obtenir la latitude et la longitude du clic
-    const latitude = evt.lngLat.lat;
-    const longitude = evt.lngLat.lng;
-
-    console.log(evt.lngLat.lat);
-    console.log(evt.lngLat.lng);
-
-    // Assigner les valeurs aux champs de latitude et longitude du formulaire
+  handleMapClick(event) {
+    const latitude = event.lngLat.lat;
+    const longitude = event.lngLat.lng;
     this.form.elLat.value = latitude;
     this.form.elLon.value = longitude;
   }
 
-  // Ajouter le formulaire à la carte
-  addFormulaire() {
+  addForm() {
     this.form = new Form();
     this.form.createDom();
     this.form.onSubmit(this.handleFormSubmit.bind(this));
   }
 
-  // Gérer la soumission du formulaire
   handleFormSubmit(event) {
-    // Récupérer les valeurs du formulaire
     const title = this.form.elTitle.value;
     const description = this.form.elDescription.value;
     const latitude = this.form.elLat.value;
@@ -86,28 +59,46 @@ class App {
     const startDate = new Date(this.form.elDebut.value);
     const endDate = new Date(this.form.elFin.value);
 
-    // Calculer la couleur de la punaise en fonction de la date
-
-    const currentDate = new Date(); // Obtenir la date et l'heure actuelles
-    const timeDifference = startDate.getTime() - currentDate.getTime(); // Différence de temps en millisecondes
-
+    const currentDate = new Date();
+    const timeDifference = startDate.getTime() - currentDate.getTime();
     const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
-    // Différence en jours en arrondissant à l'entier supérieur
 
     let markerColor;
-
     if (daysDifference > 3) {
-      // Événement dans plus de 3 jours (vert)
       markerColor = "#008000"; // Vert
     } else if (daysDifference <= 3 && daysDifference > 0) {
-      // Événement dans 3 jours ou moins (orange)
       markerColor = "#FFA500"; // Orange
     } else {
-      // Événement dépassé (rouge)
       markerColor = "#FF0000"; // Rouge
     }
 
-    // Créer un nouveau marqueur
+    const reminder = {
+      title,
+      description,
+      latitude,
+      longitude,
+      startDate: this.form.toISOString(startDate),
+      endDate: this.form.toISOString(endDate),
+      markerColor,
+    };
+
+    this.addReminderToMap(reminder);
+    this.form.saveReminder(reminder);
+    this.form.reset();
+    this.map.resize();
+    event.preventDefault();
+  }
+
+  addReminderToMap(reminder) {
+    const {
+      title,
+      description,
+      latitude,
+      longitude,
+      startDate,
+      endDate,
+      markerColor,
+    } = reminder;
     const marker = new mapboxgl.Marker({ color: markerColor })
       .setLngLat([longitude, latitude])
       .setPopup(
@@ -115,24 +106,18 @@ class App {
           `<h3>${title}</h3><p>${description}</p><p>De ${startDate} à ${endDate}</p>`
         )
       );
-
-    // Ajouter le marqueur à la carte
     marker.addTo(this.map);
+  }
 
-    // Réinitialiser les valeurs du formulaire
-    this.form.reset();
-
-    // Forcer le redimensionnement de la carte
-    this.map.resize();
-
-    // Empêcher le comportement par défaut du formulaire
-    event.preventDefault();
+  loadSavedReminders() {
+    const savedReminders = this.form.getSavedReminders();
+    savedReminders.forEach((reminder) => {
+      this.addReminderToMap(reminder);
+    });
   }
 }
 
 class Form {
-  // Propriétés des éléments du DOM
-
   elTitle;
   elDescription;
   elDebut;
@@ -142,30 +127,24 @@ class Form {
   elBtnAdd;
   elBtnSupprimer;
 
-  // Méthode qui crée le formulaire
   createDom() {
-    // Création de la div contenant le formulaire
     const formDiv = document.createElement("div");
-    formDiv.classList.add("form-container"); // Ajouter la classe pour la marge du formulaire
+    formDiv.classList.add("form-container");
 
-    // Création du formulaire
     const form = document.createElement("form");
 
-    // Création du titre
     const titleLabel = document.createElement("label");
     titleLabel.textContent = "Titre de l'événement :";
     this.elTitle = document.createElement("input");
     form.appendChild(titleLabel);
     form.appendChild(this.elTitle);
 
-    // Création de la description
     const descriptionLabel = document.createElement("label");
     descriptionLabel.textContent = "Description de l'événement :";
     this.elDescription = document.createElement("textarea");
     form.appendChild(descriptionLabel);
     form.appendChild(this.elDescription);
 
-    // Création du champ de date de début
     const startDateLabel = document.createElement("label");
     startDateLabel.textContent = "Date de début :";
     this.elDebut = document.createElement("input");
@@ -173,7 +152,6 @@ class Form {
     form.appendChild(startDateLabel);
     form.appendChild(this.elDebut);
 
-    // Création du champ de date de fin
     const endDateLabel = document.createElement("label");
     endDateLabel.textContent = "Date de fin :";
     this.elFin = document.createElement("input");
@@ -181,7 +159,6 @@ class Form {
     form.appendChild(endDateLabel);
     form.appendChild(this.elFin);
 
-    // Création du champ de latitude
     const latitudeLabel = document.createElement("label");
     latitudeLabel.textContent = "Latitude :";
     this.elLat = document.createElement("input");
@@ -189,7 +166,6 @@ class Form {
     form.appendChild(latitudeLabel);
     form.appendChild(this.elLat);
 
-    // Création du champ de longitude
     const longitudeLabel = document.createElement("label");
     longitudeLabel.textContent = "Longitude :";
     this.elLon = document.createElement("input");
@@ -197,26 +173,20 @@ class Form {
     form.appendChild(longitudeLabel);
     form.appendChild(this.elLon);
 
-    // Ajout du bouton Ajouter
     this.elBtnAdd = document.createElement("button");
     this.elBtnAdd.textContent = "Ajouter";
     this.elBtnAdd.classList.add("btn", "btn-success");
     form.appendChild(this.elBtnAdd);
 
-    // Ajout du bouton Supprimer
     this.elBtnSupprimer = document.createElement("button");
     this.elBtnSupprimer.textContent = "Supprimer";
     this.elBtnSupprimer.classList.add("btn", "btn-danger");
     form.appendChild(this.elBtnSupprimer);
 
-    // Ajout du formulaire à la div
     formDiv.appendChild(form);
-
-    // Ajout du formulaire au DOM
     document.body.appendChild(formDiv);
   }
 
-  // Méthode pour réinitialiser les valeurs du formulaire
   reset() {
     this.elTitle.value = "";
     this.elDescription.value = "";
@@ -226,12 +196,25 @@ class Form {
     this.elLon.value = "";
   }
 
-  // Méthode pour écouter la soumission du formulaire
   onSubmit(callback) {
     this.elBtnAdd.addEventListener("click", callback);
   }
+
+  saveReminder(reminder) {
+    const savedReminders = this.getSavedReminders();
+    savedReminders.push(reminder);
+    localStorage.setItem("reminders", JSON.stringify(savedReminders));
+  }
+
+  getSavedReminders() {
+    const reminders = localStorage.getItem("reminders");
+    return reminders ? JSON.parse(reminders) : [];
+  }
+
+  toISOString(date) {
+    return date.toISOString().slice(0, 10);
+  }
 }
 
-// Créer une instance de l'application et l'exporter
 const app = new App();
-export default app;
+app.start();
